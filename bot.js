bb03f6358a18bd500b83a9163c0c276e2274060e
jgbotelho3 <jgbotelho3@outlook.com>
2020-06-20 19:18:41 -0300

    Test3

--- bot.js
+++ bot.js
@@ -3,17 +3,19 @@ module.exports = () => {
   const token = "1089784292:AAFF4ilsJRCbE57XGQ1fR3H7U_r41yCZg4Y";
   const bot = new TelegramBot(token, { polling: true });
 
-  bot.onText(/\/echo (.+)/, (msg, match) => {
-    console.log(msg);
-    const chatId = msg.chat.id;
-    const resp = match[1]; // the captured "whatever"
-    bot.sendMessage(chatId, resp);
-  });
+  // bot.onText(/\/echo (.+)/, (msg, match) => {
+  //   console.log(msg);
+  //   const chatId = msg.chat.id;
+  //   const resp = match[1]; // the captured "whatever"
+  //   bot.sendMessage(chatId, resp);
+  // });
 
   bot.on("message", (msg) => {
-    console.log(msg);
     const chatId = msg.chat.id;
 
-    bot.sendMessage(chatId, "Received your message");
+    bot.sendMessage(
+      chatId,
+      "Oi, obrigado por falar comigo! Ah antes que eu me esqueça, eu amo você!"
+    );
   });
 };

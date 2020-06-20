cc7ccbb5ef2bf99bdda73c4772e52c11fb1d9cff
jgbotelho3 <jgbotelho3@outlook.com>
2020-06-20 19:15:07 -0300

    Test

--- bot.js
+++ bot.js
@@ -1,12 +1,19 @@
 const TelegramBot = require("node-telegram-bot-api");
 module.exports = () => {
   const token = "1089784292:AAFF4ilsJRCbE57XGQ1fR3H7U_r41yCZg4Y";
-
   const bot = new TelegramBot(token, { polling: true });
 
+  bot.onText(/\/echo (.+)/, (msg, match) => {
+    console.log(msg);
+    const chatId = msg.chat.id;
+    const resp = match[1]; // the captured "whatever"
+    bot.sendMessage(chatId, resp);
+  });
+
   bot.on("message", (msg) => {
-    const chat_id = msg.chat.id;
-    const echo = msg.text;
-    bot.sendMessage(chat_id, echo);
+    console.log(msg);
+    const chatId = msg.chat.id;
+
+    bot.sendMessage(chatId, "Received your message");
   });
 };
--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const http = require("http");
 
 const port = process.env.PORT || 5000;
 
-require("./bot");
+require("./bot.js");
 http
   .createServer((request, response) => {
     response.writeHead(200, { "Content-Type": "application/json" });
